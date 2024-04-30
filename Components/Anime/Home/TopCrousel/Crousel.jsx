a77b94b0de339dd0c86082521782adbd902d4622
maropboia <164220066+maropboia@users.noreply.github.com>
2024-04-30 23:04:32 +0600

    comment

--- Components/Anime/Home/TopCrousel/Crousel.jsx
+++ Components/Anime/Home/TopCrousel/Crousel.jsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import { Dimensions } from "react-native";
 import Carousel from 'react-native-reanimated-carousel';
++// Import EachCrousel component for rendering carousel items
 import { EachCrousel } from "./EachCrousel";
++// Import FormatRating utility for formatting ratings
 import FormatRating from "../../../../Utils/FormatRating";
++// Import memo for performance optimization
 import { memo } from "react";
++// Import SimpleLoading component for displaying loading state
 import SimpleLoading from "../../../Global/Loading/SimpleLoading";
 
+-// Functional component Crousel receives isLoading and Data props
 function Crousel({isLoading, Data}) {
+-// Get window width
   const width = Dimensions.get('window').width;
   return (
     <>
++// If isLoading is true, render SimpleLoading component
       {isLoading && <SimpleLoading containerStyle={{height:width / 1.2}} text={"Getting Anime"}/>}
++// If isLoading is false, render the Carousel component
       {!isLoading &&  <Carousel
         pagingEnabled={true}
         windowSize={3}
@@ -22,6 +30,7 @@ function Crousel({isLoading, Data}) {
         data={Data}
         snapEnabled={true}
         scrollAnimationDuration={1000}
++// Render EachCrousel component for each item in the data array
         renderItem={({item,index}) => {
           return <EachCrousel data={item}
                               id={item?.id ?? 0}
@@ -38,4 +47,5 @@ function Crousel({isLoading, Data}) {
   );
 }
 
++// Export memoized Crousel component
 export default memo(Crousel);

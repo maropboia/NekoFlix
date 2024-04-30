ed84e64dbaab918f7762c8012c628e0e221daaba
maropboia <164220066+maropboia@users.noreply.github.com>
2024-04-30 23:04:43 +0600

    comment

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
+// Import the `getDefaultConfig` and `mergeConfig` functions from the
+// `@react-native/metro-config` package. These functions are used to
+// configure the Metro bundler for the project.
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
+// Define an empty configuration object for the Metro bundler. This object
+// will be merged with the default configuration to create the final
+// configuration for the bundler.
 const config = {};
 
+// Merge the default configuration with the custom configuration object
+// using the `mergeConfig` function. The resulting merged configuration
+// will be used by the Metro bundler to bundle the JavaScript code for
+// the project.
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+

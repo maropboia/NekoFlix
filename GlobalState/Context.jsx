143a4d7dae844a200ea709031960183dd493b88c
maropboia <164220066+maropboia@users.noreply.github.com>
2024-04-30 23:04:30 +0600

    comment

--- GlobalState/Context.jsx
+++ GlobalState/Context.jsx
@@ -1,5 +1,9 @@
+// Import the 'createContext' function from the 'react' library
 import {createContext} from "react";
 
+// Create a new context using the 'createContext' function
 const Context = createContext()
 
+// Export the created context as the default export
 export default Context
+

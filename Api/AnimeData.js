1cfd730ac19da734367b449642d9c661f8ae7c34
maropboia <164220066+maropboia@users.noreply.github.com>
2024-04-30 23:04:32 +0600

    comment

--- Api/AnimeData.js
+++ Api/AnimeData.js
@@ -1,128 +1,80 @@
 import axios from "axios";
 import { GetServer } from "../LocalStorage/AppSettings";
+
+// This function retrieves the URL of the server based on the server name stored in LocalStorage
 async function getServerUrl(){
-  const temp = await GetServer()
-  if (temp === "Server 1"){
-    return "https://anime-api-rest.vercel.app/"
-  } else if (temp === "Server 2"){
-    return  "https://anime-api-rest-sigma.vercel.app/"
+  const temp = await GetServer() // Call the GetServer function to get the server name from LocalStorage
+  if (temp === "Server 1"){ // If the server name is "Server 1"
+    return "https://anime-api-rest.vercel.app/" // Return the URL of Server 1
+  } else if (temp === "Server 2"){ // If the server name is "Server 2"
+    return  "https://anime-api-rest-sigma.vercel.app/" // Return the URL of Server 2
   }
 }
+
+// This function retrieves the popular anime data from the server
 async function getPopularAnime(){
-  const baseUrl = await getServerUrl()
-  let config = {
+  const baseUrl = await getServerUrl() // Get the server URL using the getServerUrl function
+  let config = { // Configure the axios request
     method: 'get',
     maxBodyLength: Infinity,
     url: baseUrl + "meta/anilist/popular",
     headers: { },
   };
   try {
-    const response = await axios.request(config);
-    return response.data
+    const response = await axios.request(config); // Send the axios request
+    return response.data // Return the response data
   }
   catch (error) {
-    throw error
+    throw error // If there is an error, throw it
   }
 }
 
+// This function retrieves the trending anime data from the server
 async function getTrendingAnime(){
-  const baseUrl = await getServerUrl()
-  let config = {
+  const baseUrl = await getServerUrl() // Get the server URL using the getServerUrl function
+  let config = { // Configure the axios request
     method: 'get',
     maxBodyLength: Infinity,
     url: baseUrl + "meta/anilist/trending",
     headers: { },
   };
   try {
-    const response = await axios.request(config);
-    return response.data
+    const response = await axios.request(config); // Send the axios request
+    return response.data // Return the response data
   }
   catch (error) {
-    throw error
+    throw error // If there is an error, throw it
   }
 }
+
+// This function retrieves the airing schedule anime data from the server
 async function getAiringScheduleAnime(){
-  const baseUrl = await getServerUrl()
-  let config = {
+  const baseUrl = await getServerUrl() // Get the server URL using the getServerUrl function
+  let config = { // Configure the axios request
     method: 'get',
     maxBodyLength: Infinity,
     url: baseUrl + "meta/anilist/airing-schedule",
     headers: { },
   };
   try {
-    const response = await axios.request(config);
-    return response.data
+    const response = await axios.request(config); // Send the axios request
+    return response.data // Return the response data
   }
   catch (error) {
-    throw error
+    throw error // If there is an error, throw it
   }
 }
 
+// This function retrieves the anime info data from the server based on the anime ID
 async function getAnimeInfo(id){
-  const baseUrl = await getServerUrl()
-  let config = {
+  const baseUrl = await getServerUrl() // Get the server URL using the getServerUrl function
+  let config = { // Configure the axios request
     method: 'get',
     maxBodyLength: Infinity,
     url: baseUrl + "meta/anilist/data/" + id,
     headers: { },
   };
   try {
-    const response = await axios.request(config);
-    return response.data
-  }
-  catch (error) {
-    throw error
-  }
-}
-
-async function getAnimeEpisodes(id){
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: `https://airin.dill.moe/api/episode/${id}?releasing=false&refresh=false`,
-    headers: { },
-  };
-  try {
-    const response = await axios.request(config);
-    return response.data
-  }
-  catch (error) {
-    throw error
-  }
-}
-
-async function getAnimeEpisodesStreamingLink(id){
-  const baseUrl = await getServerUrl()
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: baseUrl + "meta/anilist/watch/" + id,
-    headers: { },
-  };
-  try {
-    const response = await axios.request(config);
-    return response.data
-  }
-  catch (error) {
-    throw error
-  }
-}
-
-async function getSearchAnime(search){
-  const baseUrl = await getServerUrl()
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: baseUrl + "meta/anilist/" + search,
-    headers: { },
-  };
-  try {
-    const response = await axios.request(config);
-    return response.data
-  }
-  catch (error) {
-    throw error
-  }
-}
-
-export {getPopularAnime, getTrendingAnime, getAiringScheduleAnime, getAnimeInfo, getAnimeEpisodes, getAnimeEpisodesStreamingLink, getSearchAnime}
+    const response = await axios.request(config); // Send the axios request
+    return response.data // Return the response data
+ 

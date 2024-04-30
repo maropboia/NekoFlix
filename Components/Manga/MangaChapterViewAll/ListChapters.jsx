c44abf82d504646da01663fcb7ffde2fd4242336
maropboia <164220066+maropboia@users.noreply.github.com>
2024-04-30 23:04:42 +0600

    comment

--- Components/Manga/MangaChapterViewAll/ListChapters.jsx
+++ Components/Manga/MangaChapterViewAll/ListChapters.jsx
@@ -1,17 +1,44 @@
 import { memo, useEffect, useState } from "react";
-import { PaddingConatiner } from "../../../Layout/PaddingConatiner";
-import { Dimensions, FlatList} from "react-native";
+import {
+  PaddingConatiner,
+  Dimensions,
+  FlatList,
+} from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { EachMangaChapterCard } from "../EachMangaChapterCard";
 
-export const ListChapters = memo(({ data, accending, image, SearchText, MangaSlug, MangaId, name, imageId}) => {
+// ListChapters is a memoized component that takes the following props:
+// data: The array of chapter data
+// accending: A boolean that determines whether the list should be sorted in ascending or descending order
+// image: The URL of the manga cover image
+// SearchText: The text used for filtering the chapter list
+// MangaSlug: The slug of the manga
+// MangaId: The ID of the manga
+// name: The name of the manga
+// imageId: The ID of the manga cover image
+export const ListChapters = memo(({ 
+  data, 
+  accending, 
+  image, 
+  SearchText, 
+  MangaSlug, 
+  MangaId, 
+  name, 
+  imageId
+}) => {
+  // Get the window width
   const { width } = Dimensions.get('window');
+  // State to store the filtered chapter data
   const [Data, setData] = useState(data);
+  // State to store the sorting order
   const [accen, setAccen] = useState(accending);
 
+  // useEffect to update the sorting order when accending changes
   useEffect(() => {
     setAccen(accending)
   }, [accending]);
+
+  // useEffect to filter the chapter data when SearchText changes
   useEffect(() => {
     if (SearchText === ""){
       setData(data)
@@ -20,19 +47,36 @@ export const ListChapters = memo(({ data, accending, image, SearchText, MangaSlu
     }
   }, [SearchText]);
 
+  // Render the chapter list using a FlatList component
   return (
     <PaddingConatiner>
-      <FlatList decelerationRate={'fast'} initialNumToRender={10} numColumns={3} showsVerticalScrollIndicator={false} data={accen ? Data : Data.reverse()} contentContainerStyle={{justifyContent:"space-between", paddingBottom:160}}
-                renderItem={({item, index})=>{
-                  if (index > 20){
-                    return <EachMangaChapterCard name={name} MangaSlug={MangaSlug} MangaId={MangaId} key={item.id} id={item.id} image={image} slug={item.slug} chapter_number={item.chapter_number} created_at={item.created_at} ContainerStyle={{marginHorizontal:width / 90}}/>
-                  } else {
-                    return <Animated.View entering={FadeInDown.delay((index < 20) ? (index * 40) : (0))} style={{marginHorizontal:width / 90}}>
-                      <EachMangaChapterCard imageId={imageId} name={name} MangaSlug={MangaSlug} MangaId={MangaId} key={item.id} id={item.id} image={image} slug={item.slug} chapter_number={item.chapter_number} created_at={item.created_at}/>
-                    </Animated.View>
-                  }
-                }}
+      <FlatList 
+        // Set the deceleration rate to 'fast'
+        decelerationRate={'fast'}
+        // Set the initial number of items to render to 10
+        initialNumToRender={10}
+        // Set the number of columns to 3
+        numColumns={3}
+        // Hide the vertical scroll indicator
+        showsVerticalScrollIndicator={false}
+        // Set the data prop to the sorted chapter data
+        data={accen ? Data : Data.reverse()}
+        // Set the content container style to justify the items and add padding at the bottom
+        contentContainerStyle={{justifyContent:"space-between", paddingBottom:160}}
+        // Render each chapter as a EachMangaChapterCard component
+        renderItem={({item, index})=>{
+          // If the index is greater than 20, add a margin to the left and right of the card
+          if (index > 20){
+            return <EachMangaChapterCard name={name} MangaSlug={MangaSlug} MangaId={MangaId} key={item.id} id={item.id} image={image} slug={item.slug} chapter_number={item.chapter_number} created_at={item.created_at} ContainerStyle={{marginHorizontal:width / 90}}/>
+          } else {
+            // Otherwise, animate the card entering the screen using FadeInDown animation
+            return <Animated.View entering={FadeInDown.delay((index < 20) ? (index * 40) : (0))} style={{marginHorizontal:width / 90}}>
+              <EachMangaChapterCard imageId={imageId} name={name} MangaSlug={MangaSlug} MangaId={MangaId} key={item.id} id={item.id} image={image} slug={item.slug} chapter_number={item.chapter_number} created_at={item.created_at}/>
+            </Animated.View>
+          }
+        }}
       />
     </PaddingConatiner>
   );
 });
+
